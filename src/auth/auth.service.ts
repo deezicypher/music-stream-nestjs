a8656a089b68c12add970297682252905c45f735
Deezi <amuhdavid@gmail.com>
2025-10-22 23:21:03 +0100

    auth guard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,18 +2,22 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { LoginDTO } from './dto/login-dto';
 import * as bcrypt from 'bcryptjs'
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class AuthService {
-  constructor(private userService:UsersService){}
-  async login(loginDTO:LoginDTO){
+  constructor(private userService:UsersService,
+    private jwtService:JwtService
+  ){}
+  async login(loginDTO:LoginDTO):Promise<{access_token:string}>{
     const user = await this.userService.findOne(loginDTO)
     const passwordMarched = await bcrypt.compare(loginDTO.password,user.password)
 
     if(passwordMarched){
-        const {password, ...safeUser} = user
-        return safeUser
+     
+        const payload = {email:user.email,sub:user.id}
+        return {access_token:this.jwtService.sign(payload)}
     }else{
         throw new UnauthorizedException('Password does not match')
     }
