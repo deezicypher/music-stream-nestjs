3bcc7e28b6b782d9af73ca2981031c961b324940
Deezi <amuhdavid@gmail.com>
2025-10-28 04:34:09 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     name: "JWT",
     description: "Enter JWT token",
     in: "header",
-  },"JWT-Auth")
+  },"JWT-auth")
   .build()
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api',app, document)
