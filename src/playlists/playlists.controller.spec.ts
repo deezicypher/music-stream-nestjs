062dd8cec15ebfa6ac99d770346eb81032eb5576
Deezi <amuhdavid@gmail.com>
2025-11-11 09:37:49 +0100

    update on playlist controller test

--- src/playlists/playlists.controller.spec.ts
+++ src/playlists/playlists.controller.spec.ts
@@ -9,9 +9,25 @@ describe('PlaylistsController', () => {
   let controller: PlaylistsController;
   let mockingPlaylistService:Partial<PlaylistsService>
 
+  const mockPlaylist = {
+    id:1,
+    name: "life is good",
+    songs:[2],
+    user: 1
+  }
+    const mockPlaylistArray = [{
+    id:1,
+    name: "life is good",
+    songs:[2],
+    user: 1
+  }]
+
   beforeEach(async () => {
     mockingPlaylistService = {
-      create:jest.fn().mockImplementation((createPlaylistDTO:CreatePlayListDto) => createPlaylistDTO)  
+      create:jest.fn().mockImplementation((createPlaylistDTO:CreatePlayListDto) => createPlaylistDTO),
+      findOne:jest.fn().mockResolvedValue(mockPlaylist),
+      findAll:jest.fn().mockResolvedValue(mockPlaylistArray),
+      findUserPlaylist:jest.fn().mockResolvedValue(mockPlaylistArray)
     }
 
     const module: TestingModule = await Test.createTestingModule({
@@ -40,5 +56,22 @@ describe('PlaylistsController', () => {
        const newplaylist = await controller.create(playlist)
        expect(newplaylist).toEqual(playlist)
        expect(mockingPlaylistService.create).toHaveBeenCalledWith(playlist)
+  })
+
+  it('returns all playlists', async () => {
+    const result = await controller.findAll()
+    expect(result).toEqual(mockPlaylistArray)
+    expect(mockingPlaylistService.findAll).toHaveBeenCalled()
+  })
+
+  it('returns a playlist', async () => {
+    const result = await controller.findOne(1)
+    expect(result).toEqual(mockPlaylist)
+    expect(mockingPlaylistService.findOne).toHaveBeenCalledWith(1)
+  })
+
+  it('it calls user playlist method', async () => {
+    const result = await controller.findUserPlaylist({user:{id:1}})
+    expect(result).toEqual(mockPlaylistArray)
   })
 });
