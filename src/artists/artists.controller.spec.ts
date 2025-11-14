a7e87c313baf52e19589bfe9085f7eff95c65b94
Deezi <amuhdavid@gmail.com>
2025-11-14 01:00:29 +0100

    artist controller create test

--- src/artists/artists.controller.spec.ts
+++ src/artists/artists.controller.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArtistsController } from './artists.controller';
+import { ArtistsService } from './artists.service';
 
 describe('ArtistsController', () => {
   let controller: ArtistsController;
+  let mockArtistService: Partial<ArtistsService>;
+
+   
+  const artist = {
+    user:1,
+    songs:[1]
+  }
 
   beforeEach(async () => {
+    mockArtistService = {
+      create:jest.fn().mockImplementation((artistDTO) => artistDTO),
+      findArtist: jest.fn().mockResolvedValue(artist)
+    }
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ArtistsController],
+      providers:[
+        {
+          provide:ArtistsService,
+          useValue: mockArtistService
+        }
+      ]
     }).compile();
 
     controller = module.get<ArtistsController>(ArtistsController);
@@ -15,4 +33,10 @@ describe('ArtistsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create an artist', async () => {
+    const result = controller.create(artist)
+    expect(mockArtistService.create).toHaveBeenCalledWith(artist)
+    expect(result).toEqual(result)
+  })
 });
