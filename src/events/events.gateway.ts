018bf8ce040910325df728cf213d486d7bb3fd16
Deezi <amuhdavid@gmail.com>
2025-11-15 06:54:57 +0100

    update

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,9 +1,33 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { OnModuleInit } from '@nestjs/common';
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { of } from 'rxjs';
+import {Server} from 'socket.io'
 
-@WebSocketGateway()
-export class EventsGateway {
+
+@WebSocketGateway({
+  cors:{
+    origin:'*'
+  }
+})
+export class EventsGateway implements OnModuleInit{
+  @WebSocketServer()
+  server:Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log(socket.connected);
+    })
+
+  }
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  handleMessage(
+    @MessageBody()
+    data: any
+  ){
+    console.log('message is recieved from client')
+    console.log(data)
+    return of({ event: 'message', data: 'Learn Node' });
   }
 }
