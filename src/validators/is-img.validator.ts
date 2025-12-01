5bc5b6b067a7e91a1cec78cd84b913515736f28d
Deezi <amuhdavid@gmail.com>
2025-12-01 21:20:47 +0100

    img validator

---
+++ src/validators/is-img.validator.ts
@@ -0,0 +1,19 @@
+import { FileValidator } from '@nestjs/common';
+
+
+
+export class IsImgValidator extends FileValidator {
+  constructor() {
+    super({});
+  }
+
+  isValid(file: Express.Multer.File): boolean {
+    const allowedTypes = ['image/png','image/jpeg', 'image/jpg','image/webp']
+    
+    return allowedTypes.includes(file.mimetype)
+  }
+
+  buildErrorMessage(): string {
+    return 'Only valid image types are allowed (png, jpeg, jpg, webp)';
+  }
+}
